7fd0ce9697bc0a407efe329ed1e693ee57571ad0
alexiscaspell <alexiscaspell@gmail.com>
2022-11-04 17:05:50 -0300

    Se especifican response types

--- apps/routes/v1/gym_route.py
+++ apps/routes/v1/gym_route.py
@@ -1,12 +1,11 @@
-from http import HTTPStatus
-
 from apps.utils.rest_util import get_valid_rest_object,get_body
 from apps.models.gym.gym_search import GymSearch
 import apps.services.gym_service as gym_service
 import apps.services.user_service as user_service
 from fastapi import APIRouter
-from fastapi import Request, FastAPI
-
+from fastapi import Request
+from typing import List
+from apps.models.gym.gym import GymClass,Gym
 
 
 import apps.configs.configuration as conf
@@ -18,15 +17,15 @@ VERSION = "/v1"
 blue_print = APIRouter(prefix=conf.get(Vars.API_BASE_PATH)+VERSION+URI)
 
 
-@blue_print.get('')
+@blue_print.get('', response_model=List[Gym])
 def get_gyms():
     return get_valid_rest_object(gym_service.get_all_gyms())
 
-@blue_print.post('/search')
+@blue_print.post('/search', response_model=List[Gym])
 def gym_search(search:GymSearch):
     return get_valid_rest_object(gym_service.get_gyms(search))
 
-@blue_print.get('/{gym_id}/classes')
+@blue_print.get('/{gym_id}/classes', response_model=List[GymClass])
 def get_classes(gym_id):
     gym = gym_service.get_gym(gym_id)
 
--- apps/routes/v1/user_route.py
+++ apps/routes/v1/user_route.py
@@ -1,11 +1,11 @@
-from http import HTTPStatus
-
 from fastapi import APIRouter
 from apps.utils.rest_util import get_valid_rest_object
 import apps.services.user_service as user_service
 
 import apps.configs.configuration as conf
 from apps.configs.vars import Vars
+from apps.models.gym.gym import GymClass
+from typing import List
 
 URI = "/users"
 VERSION = "/v1"
@@ -14,7 +14,7 @@ VERSION = "/v1"
 blue_print = APIRouter(prefix=conf.get(Vars.API_BASE_PATH)+VERSION+URI)
 
 
-@blue_print.get('/<user_id>/classes')
+@blue_print.get('/<user_id>/classes', response_model=List[GymClass])
 def get_user_classes(user_id):
     user = user_service.get_user(user_id)
     return get_valid_rest_object(user.classes)
